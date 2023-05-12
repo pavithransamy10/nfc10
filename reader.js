0fa75ce2db6aa7ed38a6629ebd56901757baef4f
pavithransamy10 <pavithranarul@gmail.com>
2023-05-12 22:44:19 +0800

    first commit

--- reader.js
+++ reader.js
@@ -1,56 +1,49 @@
-  if ('NDEFReader' in window) {
+if ('NDEFReader' in window) {
   const reader = new NDEFReader();
+  let timer;
   
   // Handle errors
   reader.onerror = (event) => {
     console.log(event);
-    alert("Error scanning NFC tag. Please try again.");
   };
   
   // Handle successful scans
   reader.onreading = (event) => {
     console.log(event.message);
     
-    // Extract data from the message and determine if the food is fresh or not
-    const data = event.message.records[0].data;
-    const dateString = new TextDecoder().decode(data);
-    const expirationDate = new Date(dateString);
-    const currentTime = new Date();
-    const differenceInSeconds = (expirationDate - currentTime) / 1000;
-    
-    if (differenceInSeconds <= 10) {
-      alert("Food is fresh!");
-    } else {
-      alert("Food is fresh!");
-    }
+    alert("Food is fresh!");
+    resetTimer();
   };
-  
-   // Check for inactivity
-  let timeoutId;
-  function startTimer() {
-    timeoutId = setTimeout(() => {
-      alert("Food is not fresh!");
-    }, 10000); // 10 seconds
-  }
-
-  function resetTimer() {
-    clearTimeout(timeoutId);
-    startTimer();
-  }
-
-  startTimer();
   
   // Start scanning when the button is clicked
   const scanButton = document.getElementById('scan-button');
   scanButton.addEventListener('click', async () => {
     try {
       await reader.scan();
+      resetTimer();
     } catch (error) {
       console.log(error);
-      alert("Error scanning NFC tag. Please try again.");
     }
   });
+  
+  // Start the timer when the page is loaded
+  startTimer();
+  
+  // Reset the timer when the user interacts with the page
+  document.addEventListener('mousemove', resetTimer);
+  document.addEventListener('keypress', resetTimer);
+  
+  // Timer functions
+  function startTimer() {
+    timer = setTimeout(() => {
+      alert("Page is inactive!");
+    }, 5000);
+  }
+  
+  function resetTimer() {
+    clearTimeout(timer);
+    startTimer();
+  }
 } else {
   console.log("NFC not supported by the browser");
-  alert("NFC not supported by the browser");
 }
